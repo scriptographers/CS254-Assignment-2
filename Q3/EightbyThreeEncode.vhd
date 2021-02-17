cd4484d988a51fc41c3ae29a4f6a27df7994d500
Harshit Varma <61575521+hrshtv@users.noreply.github.com>
2021-02-17 13:37:02 +0530

    Added a few comments

--- Q3/EightbyThreeEncode.vhd
+++ Q3/EightbyThreeEncode.vhd
@@ -2,6 +2,10 @@
 library work;
 use work.all;
 
+-- The IEEE created the IEEE VHDL library and std_logic type in standard 1164. 
+-- Parts of the IEEE library can be included in an entity by inserting the following lines
+-- The std_logic_vector type is used for arrays of std_logic variables and signals
+-- The basic VHDL logic operations are defined on this type: and, nand, or, nor, xor, xnor. These must be given two arrays of the same size; they do the operation on ecah position and return another array. The not operation negates each position in the array.
 library ieee;
 use ieee.std_logic_1164.all;
 
@@ -9,7 +13,7 @@ entity EightbyThreeEncode is
 	port ( 
 		i : in std_logic_vector(7 downto 0); -- input
 		en: in std_logic; -- enable pin
-		z : out std_logic_vector(2 downto 0) -- output
+		z : out std_logic_veactor(2 downto 0) -- output
 	);
 end entity;
 
